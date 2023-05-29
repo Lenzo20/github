42f9e36a452bd4e6f3de3b50613de5a9bae88320
Lenzo <ozielbenyn08@gmail.com>
2023-05-29 13:16:20 -0300

    correctoin

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,4 @@ class App {
   }
 }
 
-
-
 export default new App().express;
--- src/routes/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const express_1 = require("express");
-const routes = (0, express_1.Router)();
-routes.get('/', () => {
-    return 'Hello, World!';
-});
-module.exports = routes;
