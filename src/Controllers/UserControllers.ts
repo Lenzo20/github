580af6f40c5b9db3c6917198cce5a143726edc9e
matheusrangel7 <matheusrangel.estudos@gmail.com>
2023-05-31 21:40:41 -0300

    UserControllers

---
+++ src/Controllers/UserControllers.ts
@@ -0,0 +1,19 @@
+class UserControllers {
+  public index() {
+    // implementação do método index
+  }
+
+  public create() {
+    // implementação do método create
+  }
+
+  public update() {
+    // implementação do método update
+  }
+
+  public delete() {
+    // implementação do método delete
+  }
+}
+
+export default new UserControllers()
