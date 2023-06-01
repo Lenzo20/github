fc4f6f84c6d0889f0fe23279b5ca9c67428f24f1
matheusrangel7 <matheusrangel.estudos@gmail.com>
2023-05-31 22:49:20 -0300

    Index

--- src/Controllers/UserControllers.ts
+++ src/Controllers/UserControllers.ts
@@ -1,6 +1,15 @@
+import { User } from '../model/User'
+import { Request, Response } from 'express'
+
 class UserControllers {
-  public async index() {
-    // implementação do método find
+  public async index(req: Request, res: Response) {
+    const users = await User.find() // Procurando todos os usuários no banco de dados
+
+    // Validando se existe algo no banco dados
+    if (!users || users === null || users === undefined)
+      return res.json({ error: 'User Not Found' })
+
+    return res.json(users)
   }
 
   public async create() {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import UserControllers from '../Controllers/UserControllers'
 
 const routes = Router()
 
 routes.get('/', (req, res) => {
   return res.json({ Get: 'Get!' })
 })
+
+routes.get('/users', UserControllers.index)
 
 routes.post('/users', (req, res) => {
   return res.json({ Post: 'Post!' })
