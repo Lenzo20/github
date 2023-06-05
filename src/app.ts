bea7e18b84eff13ecdcfe8cd3ba1af0d7bb4229b
Lenzo <ozielbenyn08@gmail.com>
2023-06-05 20:07:35 -0300

    udate

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
+import cors from 'cors'
+import 'dotenv/config'
 import express from 'express'
 import routes from './routes/routes'
 
-const app = express()
+class App {
+  express: express.Application
 
-app.use(express.json())
+  constructor() {
+    this.express = express()
 
-app.use(routes)
+    this.middleware()
+    this.router()
+  }
 
-export = app
+  private middleware(): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  private router(): void {
+    this.express.use(routes)
+  }
+}
+
+export default new App().express
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3333
 
 connection.then(() => {
   app.listen(port, () =>
-    console.log('Server listening on port/ http://127.0.1:3333', port),
+    console.log(`Server listening on port/ http://127.0.1:${port}`),
   )
   console.log('BD conenction')
 })
